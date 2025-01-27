c8edabc84288cd890de1cd46a44000bb4fcd031f
Kevin Homuth <kevin.homuth@gmx.net>
2025-01-27 16:03:12 +0100

    merge current main, remove search package

--- app/search.tsx
+++ app/search.tsx
@@ -1,13 +1,20 @@
 // app/search.tsx
 
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
 
 // Diese Komponente zeigt die Suchfunktion an, zurzeit besteht die Seite nur aus dem Text "Search Page", das zentriert dargestellt wird.
 export default function Search() {
+  const searchAPI = (text: any) => {
+    console.warn(text);
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Search Page</Text>
+      <TextInput
+        placeholder='Search'
+        onChange={(text) => searchAPI(text)}
+      />
     </View>
   );
 }
