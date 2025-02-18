0c6ee78c86e84ba8c0bc05772c65c660f9a04c49
THTV <tienhuytonny.van@haw-hamburg.de>
2025-02-18 16:46:40 +0100

    reverted changes to search file

--- app/search.tsx
+++ app/search.tsx
@@ -5,8 +5,6 @@ import { Text, View } from 'react-native';
 
 // Diese Komponente zeigt die Suchfunktion an, zurzeit besteht die Seite nur aus dem Text "Search Page", das zentriert dargestellt wird.
 export default function Search() {
-  
-
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Search Page</Text>
